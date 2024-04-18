adc4c72f8b9bdd6c21e513a1412e3a71cb051ebf
ParbssButPeter <131292376+ParbssButPeter@users.noreply.github.com>
2024-04-18 15:37:05 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,191 @@
+function bolinho_mandioca() {
+    let div = document.getElementById("bolinho-mandioca");
+    div.style.right = "0";
+    div.style.opacity = "1";
+}
+
+function bolinho_mandioca_return() {
+    let div = document.getElementById("bolinho-mandioca");
+    div.style.right = "100vw";
+    div.style.opacity = "0";
+}
+
+function torresmo() {
+    let div = document.getElementById("torresmo");
+    div.style.right = "0";
+    div.style.opacity = "1";
+}
+
+function torresmo_return() {
+    let div = document.getElementById("torresmo");
+    div.style.right = "100vw";
+    div.style.opacity = "0";
+}
+
+function pao_queijo() {
+    let div = document.getElementById("pao-queijo");
+    div.style.right = "0";
+    div.style.opacity = "1";
+}
+
+function pao_queijo_return() {
+    let div = document.getElementById("pao-queijo");
+    div.style.right = "100vw";
+    div.style.opacity = "0";
+}
+
+function pastel_angu() {
+    let div = document.getElementById("pastel-angu");
+    div.style.right = "0";
+    div.style.opacity = "1";
+}
+
+function pastel_angu_return() {
+    let div = document.getElementById("pastel-angu");
+    div.style.right = "100vw";
+    div.style.opacity = "0";
+}
+
+function tutu_feijao_linguica() {
+    let div = document.getElementById("tutu-feijao-linguica");
+    div.style.right = "0";
+    div.style.opacity = "1";
+}
+
+function tutu_feijao_linguica_return() {
+    let div = document.getElementById("tutu-feijao-linguica");
+    div.style.right = "100vw";
+    div.style.opacity = "0";
+}
+
+function frango_quiabo() {
+    let div = document.getElementById("frango-quiabo");
+    div.style.right = "0";
+    div.style.opacity = "1";
+}
+
+function frango_quiabo_return() {
+    let div = document.getElementById("frango-quiabo");
+    div.style.right = "100vw";
+    div.style.opacity = "0";
+}
+
+function feijao_tropeiro() {
+    let div = document.getElementById("feijao-tropeiro");
+    div.style.right = "0";
+    div.style.opacity = "1";
+}
+
+function feijao_tropeiro_return() {
+    let div = document.getElementById("feijao-tropeiro");
+    div.style.right = "100vw";
+    div.style.opacity = "0";
+}
+
+function leitao_pururuca() {
+    let div = document.getElementById("leitao-pururuca");
+    div.style.right = "0";
+    div.style.opacity = "1";
+}
+
+function leitao_pururuca_return() {
+    let div = document.getElementById("leitao-pururuca");
+    div.style.right = "100vw";
+    div.style.opacity = "0";
+}
+
+function doce_leite() {
+    let div = document.getElementById("doce-leite");
+    div.style.right = "0";
+    div.style.opacity = "1";
+}
+
+function doce_leite_return() {
+    let div = document.getElementById("doce-leite");
+    div.style.right = "100vw";
+    div.style.opacity = "0";
+}
+
+function queijo_goiabada() {
+    let div = document.getElementById("queijo-goiabada");
+    div.style.right = "0";
+    div.style.opacity = "1";
+}
+
+function queijo_goiabada_return() {
+    let div = document.getElementById("queijo-goiabada");
+    div.style.right = "100vw";
+    div.style.opacity = "0";
+}
+
+function ambrosia() {
+    let div = document.getElementById("ambrosia");
+    div.style.right = "0";
+    div.style.opacity = "1";
+}
+
+function ambrosia_return() {
+    let div = document.getElementById("ambrosia");
+    div.style.right = "100vw";
+    div.style.opacity = "0";
+}
+
+function bolo_fuba_queijo() {
+    let div = document.getElementById("bolo-fuba-queijo");
+    div.style.right = "0";
+    div.style.opacity = "1";
+}
+
+function bolo_fuba_queijo_return() {
+    let div = document.getElementById("bolo-fuba-queijo");
+    div.style.right = "100vw";
+    div.style.opacity = "0";
+}
+
+function couve_refogada() {
+    let div = document.getElementById("couve-refogada");
+    div.style.right = "0";
+    div.style.opacity = "1";
+}
+
+function couve_refogada_return() {
+    let div = document.getElementById("couve-refogada");
+    div.style.right = "100vw";
+    div.style.opacity = "0";
+}
+
+function angu() {
+    let div = document.getElementById("angu");
+    div.style.right = "0";
+    div.style.opacity = "1";
+}
+
+function angu_return() {
+    let div = document.getElementById("angu");
+    div.style.right = "100vw";
+    div.style.opacity = "0";
+}
+
+function arroz_carreteiro() {
+    let div = document.getElementById("arroz-carreteiro");
+    div.style.right = "0";
+    div.style.opacity = "1";
+}
+
+function arroz_carreteiro_return() {
+    let div = document.getElementById("arroz-carreteiro");
+    div.style.right = "100vw";
+    div.style.opacity = "0";
+}
+
+function farofa_banana_terra() {
+    let div = document.getElementById("farofa-banana-terra");
+    div.style.right = "0";
+    div.style.opacity = "1";
+}
+
+function farofa_banana_terra_return() {
+    let div = document.getElementById("farofa-banana-terra");
+    div.style.right = "100vw";
+    div.style.opacity = "0";
+}
